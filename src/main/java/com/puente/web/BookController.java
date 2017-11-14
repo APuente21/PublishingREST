9e318ea00976116272f04ea352eff2eb16f538c5
Andres Puente <andres.puente012@gmail.com>
2017-11-14 15:04:54 -0500

    updated config files and book controller

--- src/main/java/com/puente/web/BookController.java
+++ src/main/java/com/puente/web/BookController.java
@@ -1,5 +1,41 @@
 package com.puente.web;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.puente.domain.Book;
+import com.puente.service.BookService;
+
+@RequestMapping("/books")
+@Controller
 public class BookController {
+	private final Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    private BookService bookService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model uiModel) {
+        logger.info("Listing contacts");
+
+        List<Book> books = bookService.findAll();
+        
+        uiModel.addAttribute("books", books);
+
+        logger.info("No. of contacts: " + books.size());
+
+        return "books/list";
+    }
+   
+    @Autowired
+    public void setContactService(BookService bookService) {
+        this.bookService = bookService;
+    }
 
 }
